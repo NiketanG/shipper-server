4e26887759e0d13aabba87de5fd04302c25d28d8
Niketan Gulekar <nikegulekar@gmail.com>
2021-06-05 22:00:30 +0530

    Updated docker-compose.yml

--- src/typeorm.config.ts
+++ src/typeorm.config.ts
@@ -10,6 +10,9 @@ export default {
 	synchronize: !__prod__,
 	url: process.env.DATABASE_URL,
 	logging: false,
-	ssl: __prod__ ? { rejectUnauthorized: false } : false,
+	ssl:
+		process.env.DB_ENABLE_SSL === "true"
+			? { rejectUnauthorized: false }
+			: false,
 	migrations: [path.join(__dirname, "./migration/*")],
 } as Parameters<typeof TypeORM.createConnection>[0];
